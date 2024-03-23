b47d2108a4217b23da4e3d0da5782c97b0d0900a
jrutherfordf <jrutherfordflores@gmail.com>
2024-03-23 00:19:07 -0600

    Title 3.1, created Color.h & Vec3.h. Changed main to use those.

--- RayTracing1/Main.cpp
+++ RayTracing1/Main.cpp
@@ -1,4 +1,8 @@
+#include "Color.h"
+#include "Vec3.h"
+
 #include <iostream>
+
 
 int main() {
 
@@ -11,14 +15,20 @@ int main() {
 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-	for (int j = 0; j < image_height; ++j) 
+	for (int j = 0; j < image_height; ++j)
 		// While there's no difference in a for loop, 
 		// ++j is preferred over j++ in C++ because ++j will increment the value and return the incremented value 
 		// whereas j++ will increment the value but return the original (pre incrementation) value
 	{
+		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush; //outputs using cLog are buffered, meaning they are written to a variable and written in one go, which is more efficient than unbuffered outputs for logging.
+
 		for (int i = 0; i < image_width; ++i)
 		{
-			auto r = double(i) / (image_width - 1); //auto detects a variable type based on its initialization
+			auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+			write_color(std::cout, pixel_color);
+
+			//OLD CODE
+			/*auto r = double(i) / (image_width - 1); //auto detects a variable type based on its initialization
 			auto g = double(j) / (image_height - 1); //auto is generally only recommended for long types (not basic types like double in this case) but for the sake of of following the book's instructions i'm using auto here.
 			auto b = 0;
 
@@ -26,7 +36,9 @@ int main() {
 			int ig = static_cast<int>(255.999 * g); //static cast converts the result of [255.999 * r] from a float to an int
 			int ib = static_cast<int>(255.999 * b);
 
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			std::cout << ir << ' ' << ig << ' ' << ib << '\n'; */
+
 		}
 	}
+	std::clog << "\rDone.                   \n";
 }
