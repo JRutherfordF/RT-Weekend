028cd6e8e68a83a60553ffc65309586562ab0164
jrutherfordf <jrutherfordflores@gmail.com>
2024-05-02 11:39:06 -0600

    Title 8.2, implemented per-pixel sampling and anti-aliasing. Fixed an issue in vec3.h where operator+= and *= did not return results

--- RayTracing1/Color.h
+++ RayTracing1/Color.h
@@ -1,16 +1,24 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-#include "RTWeekend.h"
+#include "Interval.h"
 #include "Vec3.h"
 
 using color = vec3; // Since both colour and coordinate data will use vec3, aliases are used to make distinguishing them easier
 
 void write_color(std::ostream& out, color pixel_color) {
-	// Write the translated [0,255] value of each color component
-	out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-		<< static_cast<int>(255.999 * pixel_color.y()) << ' '
-		<< static_cast<int>(255.999 * pixel_color.z()) << '\n';
+	auto r = pixel_color.x();
+	auto g = pixel_color.y();
+	auto b = pixel_color.z();
+
+	// Translate the [0.1] component values to the byte range [0,255]
+	static const interval intensity(0.000, 0.999);
+	int rbyte = int(256 * intensity.clamp(r));
+	int gbyte = int(256 * intensity.clamp(g));
+	int bbyte = int(256 * intensity.clamp(b));
+
+	// Write out the pixel color components
+	out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
 
 #endif
