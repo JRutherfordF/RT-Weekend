91d6f96fb12a837296d264c81afcbe4d3bfd3124
jrutherfordf <jrutherfordflores@gmail.com>
2024-05-08 21:22:47 -0600

    Title 9.2, fixed random generation issue that made the program hang when a ray hit the objects, implemented max_depth to limit ray bounces

--- RayTracing1/Camera.h
+++ RayTracing1/Camera.h
@@ -6,9 +6,10 @@
 
 class camera {
 public:
-	double aspect_ratio = 1.0;	// Ratio of image width over height
-	int image_width = 100;		// Rendered image width in pixel count
-	int samples_per_pixel = 10;	// Count of random samples for each pixel
+	double aspect_ratio		= 1.0;	// Ratio of image width over height
+	int image_width			= 10;	// Rendered image width in pixel count
+	int samples_per_pixel	= 100;	// Count of random samples for each pixel
+	int max_depth			= 50;	// Maximum number of ray bounces into scene
 
 	void render(const hittable& world) {
 		initialize();
@@ -21,7 +22,7 @@ public:
 				color pixel_color(0, 0, 0);
 				for (int sample = 0; sample < samples_per_pixel; sample++) {
 					ray r = get_ray(i, j);
-					pixel_color += ray_color(r, world);
+					pixel_color += ray_color(r, max_depth, world);
 				}
 				write_color(std::cout, pixel_samples_scale * pixel_color);
 			}
@@ -32,9 +33,9 @@ public:
 
 private:
 	int image_height;			// Rendered image height
-	point3 center;				// Camera center
 	double pixel_samples_scale;	// Color scale factor for a sum of pixel samples
-	point3 pixel100_loc;		// Location of pixel 0,0
+	point3 center;				// Camera center
+	point3 pixel00_loc;			// Location of pixel 0,0
 	vec3 pixel_delta_u;			// Offset to pixel to the right
 	vec3 pixel_delta_v;			// Offset to pixel below
 
@@ -61,13 +62,14 @@ private:
 
 		// Calculate the location of the upper left pixel
 		auto viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-		pixel100_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+		pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 	}
 
 	ray get_ray(int i, int j) const {
 		// Construct a camera ray originating form the origin and directed at randomly sampled point around the pixel location i, j
+
 		auto offset = sample_square();
-		auto pixel_sample = pixel100_loc
+		auto pixel_sample = pixel00_loc
 			+ ((i + offset.x()) * pixel_delta_u)
 			+ ((j + offset.y()) * pixel_delta_v);
 
@@ -82,11 +84,16 @@ private:
 		return vec3(random_double() - 0.5, random_double() - 0.5, 0);
 	}
 
-	color ray_color(const ray& r, const hittable& world) const {
+	color ray_color(const ray& r, int depth, const hittable& world) const {
+		// If we've exceeded the ray bounce limit, no more light is gathered.
+		if (depth <= 0)
+			return color(0, 0, 0);
+
 		hit_record rec;
+
 		if (world.hit(r, interval(0, infinity), rec)) {
 			vec3 direction = random_on_hemisphere(rec.normal);
-			return 0.5 * ray_color(ray(rec.p, direction), world);
+			return 0.5 * ray_color(ray(rec.p, direction), depth - 1,world);
 		}
 
 		vec3 unit_direction = unit_vector(r.direction());
--- RayTracing1/Vec3.h
+++ RayTracing1/Vec3.h
@@ -97,7 +97,7 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 				u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
-inline vec3 unit_vector(vec3 v) {
+inline vec3 unit_vector(const vec3& v) {
 	return v / v.length();
 }
 
@@ -110,15 +110,15 @@ inline vec3 random_in_unit_sphere() {
 }
 
 inline vec3 random_unit_vector() {
-	return unit_vector(random_unit_vector());
+	return unit_vector(random_in_unit_sphere());
 }
 
 inline vec3 random_on_hemisphere(const vec3& normal) {
-	vec3 on_unit_sphere = random_unit_vector();
-	if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
-		return on_unit_sphere;
-	else
-		return -on_unit_sphere;
+    vec3 on_unit_sphere = random_unit_vector();
+    if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+        return on_unit_sphere;
+    else
+        return -on_unit_sphere;
 }
 
-#endif
+#endif //!VEC3_H
