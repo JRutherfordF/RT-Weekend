894cb56ed47174ca812bd3091bcb5baa759a9649
jrutherfordf <jrutherfordflores@gmail.com>
2024-05-13 19:02:51 -0600

    Title 9.5, implemented lambertian distribution for diffuse materials and gamma correction

--- RayTracing1/Color.h
+++ RayTracing1/Color.h
@@ -6,10 +6,22 @@
 
 using color = vec3; // Since both colour and coordinate data will use vec3, aliases are used to make distinguishing them easier
 
+inline double linear_to_gamma(double linear_component) {
+	if (linear_component > 0)
+		return sqrt(linear_component);
+
+	return 0;
+}
+
 void write_color(std::ostream& out, color pixel_color) {
 	auto r = pixel_color.x();
 	auto g = pixel_color.y();
 	auto b = pixel_color.z();
+
+	// Apply a linear to gamma transform for gamma 2
+	r = linear_to_gamma(r);
+	g = linear_to_gamma(g);
+	b = linear_to_gamma(b);
 
 	// Translate the [0.1] component values to the byte range [0,255]
 	static const interval intensity(0.000, 0.999);
